b39070a3e0337534b08cf5274c01e5b644a9974c
PhongHoai <phong.nhoai@gmail.com>
2024-06-09 18:10:50 +0700

    add ts

---
+++ Object Repository/Page_CURA Healthcare Service/footer_CURA Healthcare Service             _54f5c2.rs
@@ -0,0 +1,172 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>footer_CURA Healthcare Service             _54f5c2</name>
+   <tag></tag>
+   <elementGuidId>ff4757d5-2c07-44cf-9df7-45920467f17e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>footer</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Make Appointment'])[1]/following::footer[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=contentinfo</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>footer</value>
+      <webElementGuid>6a207a18-5701-4b03-b194-7b28ec41369e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+    
+        
+            
+                CURA Healthcare Service
+                
+                Atlanta 550 Pharr Road NE Suite 525Atlanta, GA 30305
+                
+                     (678) 813-1KMS
+                     info@katalon.com
+                    
+                
+                
+                
+                    
+                        
+                    
+                    
+                        
+                    
+                    
+                        
+                    
+                
+                
+                Copyright © CURA Healthcare Service 2024
+            
+        
+    
+    
+</value>
+      <webElementGuid>4f3ca016-f043-438d-ba00-d3ad17e5476a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/footer[1]</value>
+      <webElementGuid>22bec448-ae7d-480b-8ef5-d45891da3be3</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Make Appointment'])[1]/following::footer[1]</value>
+      <webElementGuid>07f21797-1602-406f-abb7-e1c2096e370c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='We Care About Your Health'])[1]/following::footer[1]</value>
+      <webElementGuid>eb0a538a-cd00-4468-ba6a-107101afa06f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//footer</value>
+      <webElementGuid>cb87b9c0-dcec-4c34-b1d0-2399c2f4235b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//footer[(text() = '
+    
+        
+            
+                CURA Healthcare Service
+                
+                Atlanta 550 Pharr Road NE Suite 525Atlanta, GA 30305
+                
+                     (678) 813-1KMS
+                     info@katalon.com
+                    
+                
+                
+                
+                    
+                        
+                    
+                    
+                        
+                    
+                    
+                        
+                    
+                
+                
+                Copyright © CURA Healthcare Service 2024
+            
+        
+    
+    
+' or . = '
+    
+        
+            
+                CURA Healthcare Service
+                
+                Atlanta 550 Pharr Road NE Suite 525Atlanta, GA 30305
+                
+                     (678) 813-1KMS
+                     info@katalon.com
+                    
+                
+                
+                
+                    
+                        
+                    
+                    
+                        
+                    
+                    
+                        
+                    
+                
+                
+                Copyright © CURA Healthcare Service 2024
+            
+        
+    
+    
+')]</value>
+      <webElementGuid>b4379c39-587d-4496-9eea-14df593e3a86</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/Test Suites/Test Suite 3.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 3</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>fbc26230-c34d-43f5-8ad5-0191394d2797</testSuiteGuid>
+   <testCaseLink>
+      <guid>ee9be6a3-2e7d-491c-a4df-1f8e7693823d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WEB/VisualTesting</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
